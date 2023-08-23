7c71afdc47e753241c502a39a7a20b329549053a
zozulya.michael <zozymisha@gmail.com>
2023-08-23 12:17:07 +0300

    Add assets, Item is configured

--- src/main/java/net/fragre/testmod/TestMod.java
+++ src/main/java/net/fragre/testmod/TestMod.java
@@ -1,6 +1,7 @@
 package net.fragre.testmod;
 
 import com.mojang.logging.LogUtils;
+import net.fragre.testmod.item.ModItems;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -17,6 +18,8 @@ public class TestMod {
 
     public TestMod() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
---
+++ src/main/java/net/fragre/testmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package net.fragre.testmod.item;
+
+import net.fragre.testmod.TestMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TestMod.MOD_ID);
+
+    public static final RegistryObject<Item> ZIRCON = ITEMS.register("zircon",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+
+}
