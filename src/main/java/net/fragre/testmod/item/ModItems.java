acb3ba24b386a4f986370da58e03613fae941d1f
zozulya.michael <zozymisha@gmail.com>
2023-08-23 18:01:21 +0300

    Eight-ball as custom item

--- src/main/java/net/fragre/testmod/item/ModItems.java
+++ src/main/java/net/fragre/testmod/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.fragre.testmod.item;
 
 import net.fragre.testmod.TestMod;
+import net.fragre.testmod.item.custom.EightBallItem;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -17,6 +18,10 @@ public class ModItems {
 
     public static final RegistryObject<Item> RAW_ZIRCON = ITEMS.register("raw_zircon",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB)));
+
+    public static final RegistryObject<Item> EIGHT_BALL = ITEMS.register("eight_ball",
+            () -> new EightBallItem(new Item.Properties().tab(ModCreativeModeTab.TEST_TAB).stacksTo(1)));
+
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
