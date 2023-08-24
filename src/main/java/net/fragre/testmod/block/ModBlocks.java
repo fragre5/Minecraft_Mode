bbc98510a3935f894a856f004f442bf3e55de0ac
zozulya.michael <zozymisha@gmail.com>
2023-08-24 13:43:32 +0300

    Added a description when hovering over an item

--- src/main/java/net/fragre/testmod/block/ModBlocks.java
+++ src/main/java/net/fragre/testmod/block/ModBlocks.java
@@ -1,6 +1,7 @@
 package net.fragre.testmod.block;
 
 import net.fragre.testmod.TestMod;
+import net.fragre.testmod.block.custom.JumpyBlock;
 import net.fragre.testmod.item.ModCreativeModeTab;
 import net.fragre.testmod.item.ModItems;
 import net.minecraft.util.valueproviders.UniformInt;
@@ -30,6 +31,10 @@ public class ModBlocks {
             () -> new DropExperienceBlock(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(6f).requiresCorrectToolForDrops(),
                     UniformInt.of(3, 7)), ModCreativeModeTab.TEST_TAB);
+
+    public static final RegistryObject<Block> JUMPY_BLOCK = registryBlock("jumpy_block",
+            () -> new JumpyBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(6f).requiresCorrectToolForDrops()), ModCreativeModeTab.TEST_TAB);
 
     private static <T extends Block> RegistryObject<T> registryBlock(String name, Supplier<T> block, CreativeModeTab tab) {
 
--- src/main/java/net/fragre/testmod/item/custom/EightBallItem.java
+++ src/main/java/net/fragre/testmod/item/custom/EightBallItem.java
@@ -1,13 +1,21 @@
 package net.fragre.testmod.item.custom;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
     public EightBallItem(Properties properties) {
@@ -17,7 +25,7 @@ public class EightBallItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
 
-        if(!level.isClientSide && interactionHand == InteractionHand.MAIN_HAND) {
+        if (!level.isClientSide && interactionHand == InteractionHand.MAIN_HAND) {
             outputNumber(player);
             player.getCooldowns().addCooldown(this, 20);
         }
@@ -25,8 +33,28 @@ public class EightBallItem extends Item {
         return super.use(level, player, interactionHand);
     }
 
+    @Override
+    public void appendHoverText(ItemStack itemStack, @Nullable Level level, List<Component> components, TooltipFlag tooltipFlag) {
+
+        if (Screen.hasShiftDown()) {
+            components.add(Component.literal("Right click to get a random number").withStyle(ChatFormatting.AQUA));
+        } else {
+            components.add(Component.literal("Press SHIFT for more info").withStyle(ChatFormatting.YELLOW).withStyle(ChatFormatting.BOLD));
+        }
+
+        super.appendHoverText(itemStack, level, components, tooltipFlag);
+    }
+
     private void outputNumber(Player player) {
-        player.sendSystemMessage(Component.literal("Your number is " + getRandomNumber()));
+        int number = getRandomNumber();
+        if (number == 4) {
+            player.sendSystemMessage(Component.literal("You win! and your number is " + number));
+            player.addEffect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 100));
+        } else {
+            player.sendSystemMessage(Component.literal("You loose! " + number));
+            player.addEffect(new MobEffectInstance(MobEffects.POISON, 100));
+        }
+
     }
 
     private int getRandomNumber() {
