0f820424898b0c871d53dd5488674b1b62b41d2a
zozulya.michael <zozymisha@gmail.com>
2023-08-28 00:37:33 +0300

    Added registration of pressing a key with subsequent display in the chat.

---
+++ src/main/java/net/fragre/testmod/util/KeyBinding.java
@@ -0,0 +1,15 @@
+package net.fragre.testmod.util;
+
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.minecraftforge.client.settings.KeyConflictContext;
+import org.lwjgl.glfw.GLFW;
+
+public class KeyBinding {
+
+    public static final String KEY_CATEGORY = "key.category.testmod.test";
+    public static final String KEY_DRINK_WATER = "key.testmod.drink_water";
+
+    public static final KeyMapping DRINKING_KEY = new KeyMapping(KEY_DRINK_WATER, KeyConflictContext.IN_GAME,
+            InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_O, KEY_CATEGORY);
+}
